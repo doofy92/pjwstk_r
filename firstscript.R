52b48342a8a5c47afd79d3f5e8ee40a72303e442
bmikolajczyk <doofy92@gmail.com>
2021-03-20 12:39:38 +0100

    add new RStudio project and script with get weather data

---
+++ firstscript.R
@@ -0,0 +1,16 @@
+install.packages("httr")
+install.packages("jsonlite")
+install.packages("rtools")
+library(httr)
+library(jsonlite)
+require(jsonlite)
+
+
+print("hello world")
+endpoint <- "https://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=1765994b51ed366c506d5dc0d0b07b77"
+getWeather <- GET(endpoint)
+getWeather$content
+weatherText <- content(getWeather,"text")
+weatherJson <- fromJSON(weatherText, flatten = TRUE)
+weatherDF <- as.data.frame(weatherJson)
+View(weatherDF)
